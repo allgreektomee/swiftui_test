d3d832422e672ef59e8a8ce99fc1d956f8de7a12
devcation <88315076+devcation-learn@users.noreply.github.com>
2023-06-05 14:19:01 +0900

    Initial Commit

---
+++ agtm/agtmApp.swift
@@ -0,0 +1,17 @@
+//
+//  agtmApp.swift
+//  agtm
+//
+//  Created by 20201385 on 2023/06/05.
+//
+
+import SwiftUI
+
+@main
+struct agtmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
